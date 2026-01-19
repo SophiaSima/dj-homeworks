7f1108fd4c9483c2386a0dbeb3471f98eb084267
Sophia <s89110227947@yandex.ru>
2026-01-19 15:53:45 +0300

    Done 2.2-databases-2/orm-migrations

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -74,6 +74,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
+        'PASSWORD': 'postrgeDB2025',
+        'USER': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
